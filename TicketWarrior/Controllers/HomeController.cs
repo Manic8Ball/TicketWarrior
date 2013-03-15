65906d2613af261c2416014d96b149fc546abe93
Steven Ellinger <ellinger96517@gotoltc.edu>
2013-03-15 16:46:04 -0500

    Added HomeController Index and simple TicketForm

---
+++ TicketWarrior/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicketWarrior.Models;
+
+namespace TicketWarrior.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ViewResult ConcertForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult ConcertForm(TicketResponse ticketResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to the party organizer
+                return View("Thanks", ticketResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+    }
+}
---
+++ TicketWarrior/Models/TicketResponse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace TicketWarrior.Models
+{
+    public class TicketResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string City
+        {
+            get;
+            set;
+        }
+        public string State
+        {
+            get;
+            set;
+        }
+        public string Email
+        {
+            get;
+            set;
+        }
+        public bool MailedOrPickUp
+        {
+            get;
+            set;
+        }
+    }
+}
