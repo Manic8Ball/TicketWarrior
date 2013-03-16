f04c0c7901a2c9a0508d817bf6ad46ac8f8a7049
Steven Ellinger <ellinger96517@gotoltc.edu>
2013-03-15 21:26:15 -0500

    added validation and css

--- TicketWarrior/Models/TicketResponse.cs
+++ TicketWarrior/Models/TicketResponse.cs
@@ -16,26 +16,36 @@ namespace TicketWarrior.Models
             set;
         }
 
+        [Required(ErrorMessage = "Please enter your address")]
         public string Address
         {
             get;
             set;
         }
+        [Required(ErrorMessage = "Please enter your city")]
         public string City
         {
             get;
             set;
         }
+
+        [Required(ErrorMessage = "Please enter your state")]
         public string State
         {
             get;
             set;
         }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",
+        ErrorMessage = "Please enter a valid email address")]
         public string Email
         {
             get;
             set;
         }
+
+        [Required(ErrorMessage = "Please choose delivery type")]
         public bool MailedOrPickUp
         {
             get;
